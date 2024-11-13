3234e9ca35bde55f02f016fa9360880e77b1fed0
Mihail <tchechin.mihael@gmail.com>
2024-11-13 14:33:49 +0300

    Ubuntu

---
+++ io/hexlet/Example.java
@@ -0,0 +1,10 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Example {
+	public static void main(String[] args) {
+		var capitalizadWord = StringUtils.capitalize("hexlet");
+		System.out.println(capitalizadWord);
+	}
+}
--- io/hexlet/example/App.java
+++
@@ -1,7 +0,0 @@
-package io.hexlet.example;
-
-public class App {
-	public static void main (String[] args) {
-		System.out.print("Hello, world!");
-	}
-}
